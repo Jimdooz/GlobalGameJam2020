20139adb7d0721740c6815980fdf556e9ed339d5
Jimdooz <romain.saclier@gmail.com>
2020-02-01 22:35:55 +0100

    Bonjour :D

--- Assets/Scripts/Vision/VisualFieldOfView.cs
+++ Assets/Scripts/Vision/VisualFieldOfView.cs
@@ -16,7 +16,10 @@ public class VisualFieldOfView : MonoBehaviour
     void Start() {
         field = GetComponent<FieldOfView>();
 
-        Light2D observerLight = gameObject.AddComponent(typeof(Light2D)) as Light2D;
+        GameObject light2D = Instantiate(Resources.Load("Prefabs/MAIN_MUSIC", typeof(GameObject))) as GameObject;
+        Light2D observerLight = light2D.GetComponent<Light2D>();
+
+        CopyComponent(observerLight, gameObject);
 
         observerLight.pointLightInnerAngle = field.viewAngle;
         observerLight.pointLightOuterAngle = field.viewAngle;
@@ -26,6 +29,17 @@ public class VisualFieldOfView : MonoBehaviour
         observerLight.color = color;
         observerLight.shadowIntensity = 1f;
         observerLight.shadowVolumeIntensity = 1f;
-        observerLight.volumeOpacity = intensity;
+    }
+
+    Component CopyComponent(Component original, GameObject destination) {
+        System.Type type = original.GetType();
+        Component copy = destination.AddComponent(type);
+        // Copied fields can be restricted with BindingFlags
+        System.Reflection.FieldInfo[] fields = type.GetFields();
+        foreach (System.Reflection.FieldInfo field in fields)
+        {
+            field.SetValue(copy, field.GetValue(original));
+        }
+        return copy;
     }
 }
