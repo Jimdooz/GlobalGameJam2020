d88c8d2ca0b5e7b01b273b63a55893698ae33535
At-lien313 <esteban.legrand@outlook.fr>
2020-02-01 10:26:04 +0100

    Ajout Player : Input et mouvement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerInputs inputScript;
+
+    [Header("Movement")]
+    [SerializeField]
+    private float moveSpeed = 1.2f;
+    private Vector2 movement;
+
+    private bool facingRight = true;
+    private bool walking;
+
+    private Rigidbody2D rb;
+
+    private Animator animator;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+        inputScript = GetComponent<PlayerInputs>();
+    }
+
+    private void Update()
+    {
+        HandleDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        movement = inputScript.normalizedDirection;
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void HandleDirection()
+    {
+        if (movement.y < 0)
+        {
+            animator.SetTrigger("front");
+            walking = true;
+        }
+        else if (movement.y > 0)
+        {
+            animator.SetTrigger("back");
+            walking = true;
+        }
+
+        else
+        {
+            if (movement.x < 0 || movement.x > 0)
+            {
+                flipPlayer();
+                animator.SetTrigger("side");
+                walking = true;
+            }
+
+            else
+            {
+                walking = false;
+            }
+        }
+
+        if (walking && !animator.GetBool("walking"))
+        {
+            animator.SetBool("walking", true);
+        }
+        else if (!walking && animator.GetBool("walking"))
+        {
+            animator.SetBool("walking", false);
+        }
+    }
+
+    void flipPlayer()
+    {
+        if (facingRight && movement.x <= 0)
+        {
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+            facingRight = !facingRight;
+        }
+        else if (!facingRight && movement.x >= 0)
+        {
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+            facingRight = !facingRight;
+        }
+    }
+}
