cdbe9b1779444ba76835e6f01feafe6d148441da
Jimdooz <romain.saclier@gmail.com>
2020-02-02 13:22:52 +0100

    Coucou

--- Assets/Scripts/Interaction/InteractionButton.cs
+++ Assets/Scripts/Interaction/InteractionButton.cs
@@ -6,15 +6,14 @@ using UnityEngine.UI;
 public class InteractionButton : MonoBehaviour
 {
     public Image button;
-    protected Animator animator;
+    public Animator animator;
 
     static protected float unit = 1 / 32f;
     static protected float diminue = 0.00005f;
     // Start is called before the first frame update
-    void Start() {
-        animator = GetComponent<Animator>();
+    void Start()
+    {
         button.rectTransform.sizeDelta = new Vector2(button.sprite.rect.width * InteractionButton.unit - InteractionButton.diminue, button.sprite.rect.height * InteractionButton.unit - InteractionButton.diminue);
-        hide();
     }
 
     // Update is called once per frame
@@ -22,24 +21,13 @@ public class InteractionButton : MonoBehaviour
     {
 
     }
-
     public void show()
     {
-        Debug.Log("SHOW");
-        if (!animator)
-        {
-            animator = GetComponent<Animator>();
-        }
         animator.SetBool("show", true);
     }
 
     public void hide()
     {
-        Debug.Log("HIDE");
-        if (!animator)
-        {
-            animator = GetComponent<Animator>();
-        }
         animator.SetBool("show", false);
     }
 }
